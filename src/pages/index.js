c85af31e84e4e0e59dc8ba6c3edee989771ab004
kysniw <kuba.winsyk.forgames@gmail.com>
2024-05-19 15:50:30 +0200

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        src="../images/sicily.jpg"
+        alt="Beautiful landscape in Sicily, Italy."
+      />
     </Layout>
   );
 };
